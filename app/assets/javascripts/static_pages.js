f167cdc831e8f60822f25bdc37a655f73df980e9
Sachin Mour <rtr.sachinmour@gmail.com>
2016-08-12 20:17:59 -0400

    insert raw html

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,2 +1,24 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).on('turbolinks:load', function(event) {
+  event.preventDefault();
+
+  function is_touch_device() {
+		return 'ontouchstart' in window // works on most browsers 
+		|| navigator.maxTouchPoints; // works on IE10/11 and Surface
+	};
+
+	if (!is_touch_device()) {
+	    $('div.info').hide();
+	    $('div.project').on({
+			mouseenter: function() {
+			    var $this = $(this);
+			    $this.find('.info').slideDown('fast');
+			},
+			mouseleave: function() {
+			    var $this = $(this);
+			    $this.find('.info').slideUp('fast');
+			}
+	    });
+	}
+
+
+});
