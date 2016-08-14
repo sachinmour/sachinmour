53bf149ef3d88cbcc88f67ea455f8a819ddc0d87
Sachin Mour <rtr.sachinmour@gmail.com>
2016-08-14 16:31:40 -0400

    Add google analytics

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,6 +1,12 @@
 $(document).on('turbolinks:load', function(event) {
   event.preventDefault();
 
+  /*global ga*/
+    if (typeof(ga) === "function") {
+        ga("set", "location", event.originalEvent.data.url);
+        ga("send", "pageview");
+    }
+
   function is_touch_device() {
 		return 'ontouchstart' in window // works on most browsers 
 		|| navigator.maxTouchPoints; // works on IE10/11 and Surface
